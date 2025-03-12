2e9a7a5e8bf22697f8cccc938753bf3f61cad0f3
Etativel <farhanmaulana4611@gmail.com>
2025-03-12 14:02:53 +0800

    feat: origin for user-frontend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,12 @@ const passport = require("passport");
 require("./services/passport");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
