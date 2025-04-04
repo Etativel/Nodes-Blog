ee719406316e5a1d1e351fde3f940c491d13756f
Etativel <farhanmaulana4611@gmail.com>
2025-04-04 15:33:03 +0800

    fix(getUserPosts): always return 200 to prevent frontend error

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -25,9 +25,7 @@ async function getUserPosts(req, res) {
         },
       },
     });
-    if (!posts || posts.length === 0) {
-      return res.status(404).json({ error: "No posts found for this user" });
-    }
+
     return res.json({ posts });
   } catch (error) {
     console.error(error);
