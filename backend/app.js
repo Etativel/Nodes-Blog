85eb7962f589a8eca3b373a53dd0f4f6a1a92a2e
Etativel <farhanmaulana4611@gmail.com>
2025-04-22 15:51:43 +0800

    feat: cors config

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,11 @@ app.use(
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://nodes-blog-production.up.railway.app/",
+      "https://nodes-blog-user-frontend.up.railway.app/",
     ],
     credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
   })
 );
 app.use(passport.initialize());
