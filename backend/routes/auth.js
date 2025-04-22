70dff5dba1990489baef29f50982d1a707cc92ab
Etativel <farhanmaulana4611@gmail.com>
2025-04-22 15:19:26 +0800

    feat: change auth login config

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,7 +47,8 @@ router.post("/login", (req, res, next) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         // maxAge: 30000,
       });
@@ -60,7 +61,8 @@ router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
   });
   res.json({ message: "Logged out successfully" });
 });
